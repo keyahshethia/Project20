19a1645b177f79841445eae89aa50bc36c304c1e
keyahshethia <71205945+keyahshethia@users.noreply.github.com>
2020-11-21 11:16:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,7 @@ var speed,weight;
 
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-}
-
-function draw() {
-  background(255,255,255); 
-  car = createSprite(1500,200,60,height/2);
+car = createSprite(1500,200,60,height/2);
 
   wall = createSprite(1500,200,60,height/2);
   
@@ -18,8 +13,12 @@ function draw() {
   weight = random(400,1500); 
   
   car.velocityX = speed;
+}
+
+  function draw() {
+  background(0); 
   
-  if(wall.x-car.x<(car.width+wall.width)/2){
+   if(wall.x-car.x<(car.width+wall.width)/2){
   car.velocityX = 0;
   var deformation = 0.5*weight*speed*speed/22509;
   if(deformation>180){
